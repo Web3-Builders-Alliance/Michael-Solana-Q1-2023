c7ae0a3c21e5c537e5aae8a5a2079110b4218f3d
rnb2022 <mdb1710@gmail.com>
2023-01-30 16:53:54 -0500

    first commit

---
+++ rustprereq/Traversy Tutorial/target/debug/deps/rustprereq-35510e3bb9b230fb.d
@@ -0,0 +1,6 @@
+/Volumes/musicmoney/codebymdb2023/WBA/rustprereq/Traversy Tutorial/target/debug/deps/rustprereq-35510e3bb9b230fb.rmeta: src/main.rs src/cli.rs
+
+/Volumes/musicmoney/codebymdb2023/WBA/rustprereq/Traversy Tutorial/target/debug/deps/rustprereq-35510e3bb9b230fb.d: src/main.rs src/cli.rs
+
+src/main.rs:
+src/cli.rs:
